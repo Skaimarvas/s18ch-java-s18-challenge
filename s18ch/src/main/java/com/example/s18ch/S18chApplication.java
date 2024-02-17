94f99fd6b65a62c9ecba09382a4432e1236721b0
skaimarvas <skaimarvas@gmail.com>
2024-02-17 19:15:28 +0300

    Readme was moved to internal file

---
+++ s18ch/src/main/java/com/example/s18ch/S18chApplication.java
@@ -0,0 +1,13 @@
+package com.example.s18ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S18chApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S18chApplication.class, args);
+	}
+
+}
---
+++ s18ch/src/test/java/com/example/s18ch/S18chApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s18ch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S18chApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
