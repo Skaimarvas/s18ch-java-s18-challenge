2578028202531f19a9f18640361dc9f3598e6f82
skaimarvas <skaimarvas@gmail.com>
2024-02-17 20:16:41 +0300

    annotations and fields were created

--- s18ch/src/main/java/com/example/s18ch/entity/Author.java
+++ s18ch/src/main/java/com/example/s18ch/entity/Author.java
@@ -1,4 +1,29 @@
 package com.example.s18ch.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "author", schema = "fsweb")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+
 }
