2c9c08ed11658fda9aa5b21ae313b0772e0cb907
skaimarvas <skaimarvas@gmail.com>
2024-02-17 20:07:20 +0300

    annotations and fields were created

--- s18ch/src/main/java/com/example/s18ch/entity/Category.java
+++ s18ch/src/main/java/com/example/s18ch/entity/Category.java
@@ -1,4 +1,29 @@
 package com.example.s18ch.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+/**
+ * Notes:
+ * category has more than one books that´s why we used @OneToMany annotation
+ */
+
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "category", schema = "fsweb")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "category_name")
+    private String categoryName;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Book> books;
 }
