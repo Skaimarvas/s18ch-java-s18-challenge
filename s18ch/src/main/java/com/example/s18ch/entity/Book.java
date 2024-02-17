5a0d84f4b57cff5c12e82fd73302c325fe631aaf
skaimarvas <skaimarvas@gmail.com>
2024-02-17 20:29:09 +0300

    annotations and fields were created and a note was added

--- s18ch/src/main/java/com/example/s18ch/entity/Book.java
+++ s18ch/src/main/java/com/example/s18ch/entity/Book.java
@@ -1,4 +1,33 @@
 package com.example.s18ch.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Notes:
+ * When we delete author , the books author wrote will delete according to CascadeType.ALL
+ *
+ */
+
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "book", schema = "fsweb")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "book_name" )
+    private String bookName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+
+
 }
